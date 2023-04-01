dc79ba1ef6d499666a21170c106a8887aa92cac0
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-01 01:31:29 -0700

    throw if missing required env vars

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,18 @@
 import { createClient } from 'contentful';
 
 
-const {
-  CONTENTFUL_SPACE_ID,
-  CONTENTFUL_ACCESS_TOKEN,
-} = process.env;
+const CONTENTFUL_SPACE_ID: string = process.env["CONTENTFUL_SPACE_ID"] as string;
+const CONTENTFUL_ACCESS_TOKEN: string = process.env["CONTENTFUL_ACCESS_TOKEN"] as string;
+
+
+if( !CONTENTFUL_SPACE_ID || !CONTENTFUL_ACCESS_TOKEN ){
+  throw new Error("Missing CONTENTFUL_SPACE_ID or CONTENTFUL_ACCESS_TOKEN");
+}
 
 
 const client = createClient({
-  space: CONTENTFUL_SPACE_ID || 'xxxxxxxx',
-  accessToken: CONTENTFUL_ACCESS_TOKEN || 'xxxxxxxx',
+  space: CONTENTFUL_SPACE_ID,
+  accessToken: CONTENTFUL_ACCESS_TOKEN,
 });
 
 
