dfcaec5c8b80443d564c4065285c058060270611
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-09 19:05:04 -0700

    Generate TS declarations for content types https://github.com/contentful-userland/cf-content-types-generator#usage ; also .. updated eslintrc.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,133 +1,96 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import ReactMarkdown from "react-markdown";
 
-import getBlogPosts from '@/utils';
+import { getBlogPosts } from "@/utils";
+import { TypeBlogPost } from "@/types";
 
-const inter = Inter({ subsets: ['latin'] })
 
-export interface BlogPost {
-  fields: {
-    title: string;
-  },
-  sys: {
-    id: string;
-  }
-}
+const inter = Inter({ subsets: ["latin"] });
+
 
 export interface HomeProps {
-  posts: BlogPost[];
+  posts: TypeBlogPost[];
 }
 
-export default function Home({ posts }: HomeProps) {
-
-  console.log({ posts });
-
-  return (
+export default function Home({ posts }: HomeProps ){
+  console.log( "home component props:", { posts });
+  return(
     <>
       <Head>
         <title>Audeos.com</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main className={ styles.main }>
+        <div className={ styles.description }>
           <p>
             Welcome to &nbsp;
-            <code className={styles.code}>Audeos.com</code>
+            <code className={ styles.code }>Audeos.com</code>
           </p>
-          <div>
-            Audeosdotcom
-          </div>
+          <div>Audeosdotcom</div>
         </div>
 
-        <div className={styles.center}>
+        <div className={ styles.center }>
           Audeos.com
-          <div className={styles.thirteen}>
-            sup
-          </div>
+          <div className={ styles.thirteen }>sup</div>
         </div>
 
         <ul>
-        {
-          posts
-            .map(
-              post =>
-                <div key={ post.sys.id }>
+          {
+            posts.map( post => {
+              console.log({ post });
+              return(
+                <div key={ post.fields.slug }>
                   <h4>{ post.fields.title }</h4>
-                  <p>
-                    {/* { documentToHtmlString(post.fields.body) } */}
-                  </p>
+                  <p>/post/{ post.fields.slug }</p>
+                  <ReactMarkdown>{ post.fields.body || "" }</ReactMarkdown>
                 </div>
-            )
-        }
+              );
+            })
+          }
         </ul>
 
-        <div className={styles.grid}>
-          <a
-            href=""
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
+        <div className={ styles.grid }>
+          <a href="" className={ styles.card } rel="noopener noreferrer">
+            <h2 className={ inter.className }>
               Foo <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Bar.
-            </p>
+            <p className={ inter.className }>Bar.</p>
           </a>
 
-          <a
-            href=""
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
+          <a href="" className={ styles.card } rel="noopener noreferrer">
+            <h2 className={ inter.className }>
               Foo <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Bar
-            </p>
+            <p className={ inter.className }>Bar</p>
           </a>
 
-          <a
-            href=""
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
+          <a href="" className={ styles.card } rel="noopener noreferrer">
+            <h2 className={ inter.className }>
               Foo <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Bar.
-            </p>
+            <p className={ inter.className }>Bar.</p>
           </a>
 
-          <a
-            href=""
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
+          <a href="" className={ styles.card } rel="noopener noreferrer">
+            <h2 className={ inter.className }>
               Foo <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Bar
-            </p>
+            <p className={ inter.className }>Bar</p>
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(){
   const posts = await getBlogPosts();
-  return {
+  return{
     props: {
-      posts
+      posts,
     },
-  }
+  };
 }
