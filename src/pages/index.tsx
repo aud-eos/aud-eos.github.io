a3d6763158745208c7caa85f3964edc93b4052a1
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-09 21:14:46 -0700

    strip all styles, sort blog posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import Link from "next/link";
+
 import styles from "@/styles/Home.module.css";
-import ReactMarkdown from "react-markdown";
-
-import { getBlogPosts } from "@/utils";
 import { TypeBlogPost } from "@/types";
+import { sortBlogPostsByDate } from "@/utils/blogPostUtils";
+import { getBlogPosts } from "@/utils/contentfulUtils";
 
-
-const inter = Inter({ subsets: ["latin"] });
 
 
 export interface HomeProps {
@@ -24,63 +22,20 @@ export default function Home({ posts }: HomeProps ){
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={ styles.main }>
-        <div className={ styles.description }>
-          <p>
-            Welcome to &nbsp;
-            <code className={ styles.code }>Audeos.com</code>
-          </p>
-          <div>Audeosdotcom</div>
-        </div>
-
-        <div className={ styles.center }>
-          Audeos.com
-          <div className={ styles.thirteen }>sup</div>
-        </div>
-
         <ul>
           {
-            posts.map( post => {
-              console.log({ post });
-              return (
-                <div key={ post.fields.slug }>
-                  <h4>{ post.fields.title }</h4>
-                  <p>/post/{ post.fields.slug }</p>
-                  <ReactMarkdown>{ post.fields.body || "" }</ReactMarkdown>
-                </div>
-              );
+            posts
+              .sort( sortBlogPostsByDate )
+              .map( post => {
+                return (
+                  <li key={ post.fields.slug }>
+                    <Link href={ `/post/${post.fields.slug}` }>Post Title: { post.fields.title }</Link>
+                    <p>Date: { new Date( post.fields.date || post.sys.createdAt ).toLocaleString() }</p>
+                  </li>
+                );
             })
           }
         </ul>
-
-        <div className={ styles.grid }>
-          <a href="" className={ styles.card } rel="noopener noreferrer">
-            <h2 className={ inter.className }>
-              Foo <span>-&gt;</span>
-            </h2>
-            <p className={ inter.className }>Bar.</p>
-          </a>
-
-          <a href="" className={ styles.card } rel="noopener noreferrer">
-            <h2 className={ inter.className }>
-              Foo <span>-&gt;</span>
-            </h2>
-            <p className={ inter.className }>Bar</p>
-          </a>
-
-          <a href="" className={ styles.card } rel="noopener noreferrer">
-            <h2 className={ inter.className }>
-              Foo <span>-&gt;</span>
-            </h2>
-            <p className={ inter.className }>Bar.</p>
-          </a>
-
-          <a href="" className={ styles.card } rel="noopener noreferrer">
-            <h2 className={ inter.className }>
-              Foo <span>-&gt;</span>
-            </h2>
-            <p className={ inter.className }>Bar</p>
-          </a>
-        </div>
       </main>
     </>
   );
