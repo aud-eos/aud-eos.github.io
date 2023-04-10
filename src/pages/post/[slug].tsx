1ca5f1a5bb0c6f8bd1e30a65b1373c8b7fe69124
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-09 19:40:33 -0700

    add yarn script `format` to run next lint --fix

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -12,18 +12,18 @@ export const BlogPostView: FC<SlugProps> = ({ post }) => {
   return (
     <>
       <Head>
-        <title>{post.title} — My Next.js Static Blog</title>
+        <title>{ post.title } — My Next.js Static Blog</title>
       </Head>
     </>
   );
 };
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getStaticProps( context: GetStaticPropsContext ){
   const slug = context.params?.slug;
-  if (typeof slug !== "string") {
+  if( typeof slug !== "string" ){
     return { props: {} };
   } else {
-    const post = await getBlogPost(slug);
+    const post = await getBlogPost( slug );
     return {
       props: {
         post,
@@ -32,13 +32,13 @@ export async function getStaticProps(context: GetStaticPropsContext) {
   }
 }
 
-export async function getStaticPaths() {
+export async function getStaticPaths(){
   const posts = await getBlogPosts();
 
   // Map the result of that query to a list of slugs.
   // This will give Next the list of all blog post pages that need to be
   // rendered at build time.
-  const paths = posts.map((post) => {
+  const paths = posts.map( post => {
     const slug = post.fields.slug;
     return { params: { slug } };
   });
