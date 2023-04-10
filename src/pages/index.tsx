282ef4fe2ccd01f69cbcb1189db84aab6378371a
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-10 10:31:08 -0700

    update favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home({ posts }: HomeProps ){
       <Head>
         <title>Audeos.com</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main className={ styles.main }>
         <BlogPostList posts={ posts } />
