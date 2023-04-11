86f98b624780c982fffcd361205f93e01e91dc35
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-11 16:13:08 -0700

    Create Picture component for responsive/optimized images

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,41 +1,7 @@
 import { FC } from "react";
 import ReactMarkdown from "react-markdown";
 import styles from "@/styles/Markdown.module.scss";
-
-interface ImageSourceSetOptions {
-  format: "jpg"|"png"|"webp"|"gif"|"avif"
-  width: number
-}
-
-
-/**
- * Return URL for image source
- * @param src
- * @param options
- * @returns string
- */
-const getImgSrcSet = ( src: string, options: ImageSourceSetOptions ): string => {
-  const imageUrl = new URL( `https:${src}` );
-  imageUrl.searchParams.set( "fm", options.format );
-
-  // Progressive JPEGs
-  // https://www.contentful.com/developers/docs/references/images-api/#/reference/changing-formats/progressive-jpegs/retrieve-an-image/console/curl
-  if( options.format === "jpg" ){
-    imageUrl.searchParams.set( "fl", "progressive" );
-  }
-
-  imageUrl.searchParams.set( "w", options.width.toString() );
-  return imageUrl.toString();
-};
-
-
-const SRCSETS: ImageSourceSetOptions[] = [
-  { format: "webp", width: 750 },
-  { format: "webp", width: 550 },
-  { format: "webp", width: 450 },
-  { format: "webp", width: 350 },
-  { format: "webp", width: 250 },
-];
+import Picture from "@/components/Picture";
 
 
 const markdownComponents: object = {
@@ -45,23 +11,12 @@ const markdownComponents: object = {
     if( node.children[0].tagName === "img" ){
       const image = node.children[0];
       const { src } = image.properties;
-      const imageSource = getImgSrcSet( src, { format: "jpg", width: 750 });
       return (
         <figure className={ styles.markdown }>
-          <picture>
-            {
-              SRCSETS.map( ( options, idx ) =>
-                <source key={ `sourceset${idx}` }
-                  media={ `(max-width: ${options.width}px)` }
-                  srcSet={ getImgSrcSet( src, options ) }
-                  type={ `image/${options.format}` }
-                  /> )
-            }
-            <img
-              src={ imageSource }
-              alt={ image.properties.alt }
+          <Picture
+            url={ src }
+            alt={ image.properties.alt }
             />
-          </picture>
           <figcaption>
             { image.properties.alt }
           </figcaption>
