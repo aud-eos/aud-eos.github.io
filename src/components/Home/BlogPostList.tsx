1689d361b97bf21ff8c9c9f06ef276487526e6f5
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-11 13:54:55 -0700

    some visual and formatting tweaks

--- src/components/Home/BlogPostList.tsx
+++ src/components/Home/BlogPostList.tsx
@@ -22,20 +22,20 @@ export default function BlogPostList({ posts }: BlogPostListProps ){
             const url = `/post/${post.fields.slug}`;
             return (
               <li key={ post.sys.id }>
-                <Link href={ url }>
                   <figure>
-                    <picture>
-                      <img
-                        src={ `https:${post.fields.image?.fields.file.url}?h=${IMAGE_SIZE}` }
-                        alt={ post.fields.image?.fields.description || "" }
-                      />
-                    </picture>
+                    <Link href={ url }>
+                      <picture>
+                        <img
+                          src={ `https:${post.fields.image?.fields.file.url}?h=${IMAGE_SIZE}` }
+                          alt={ post.fields.image?.fields.description || "" }
+                        />
+                      </picture>
+                    </Link>
                     <figcaption>
-                      <h3>{ post.fields.title }</h3>
+                      <Link href={ url }><h3>{ post.fields.title }</h3></Link>
                       <h5><DateTimeFormat timestamp={ post.fields.date || post.sys.createdAt } /></h5>
                     </figcaption>
                   </figure>
-                </Link>
               </li>
           );
         })
