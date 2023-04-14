58ec0591f39507dd59a15ac8abac73b0abe0247a
Ben Petty <22062601+benpetty@users.noreply.github.com>
2023-04-13 18:53:49 -0700

    remove commento

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -12,6 +12,15 @@ export function Comments(){
     return null;
   }
 
+  /**
+   * @TODO Commento is not being actively developed and breaks on page route
+   * changes. https://www.talkyard.io/ seems good and works with Gatsby but
+   * doesn't obviously yet work with next.js. So no comments for now.
+   */
+  if( isHydrated || true === true ){
+    return null;
+  }
+
   return (
     <>
       <Script defer src="https://cdn.commento.io/js/commento.js"></Script>
