07fe711363bc461c02854d271fb1985adb6fe71e
benny <22062601+benpetty@users.noreply.github.com>
2026-03-27 11:25:51 -0700

    update eslint and vscode config, update markdown code based on changelog

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig( [
+  ...nextVitals,
+    {
+    rules: {
+        "array-bracket-spacing": [ 2, "always" ],
+        "arrow-parens": [ 2, "as-needed" ],
+        "comma-dangle": [ "error", "always-multiline" ],
+
+        "keyword-spacing": [ 2, {
+            after: true,
+
+            overrides: {
+                if: {
+                    after: false,
+                },
+
+                for: {
+                    after: false,
+                },
+            },
+        } ],
+
+        "no-multi-spaces": [ 2 ],
+        "object-curly-spacing": [ 2, "always" ],
+
+        quotes: [ 2, "double", {
+            avoidEscape: true,
+            allowTemplateLiterals: true,
+        } ],
+
+        "react/jsx-curly-spacing": [ 2, {
+            when: "always",
+            allowMultiline: true,
+            children: true,
+
+            spacing: {
+                objectLiterals: "always",
+            },
+        } ],
+
+        semi: [ 2, "always" ],
+
+        "space-in-parens": [ 2, "always", {
+            exceptions: [ "{}" ],
+        } ],
+
+        "space-before-blocks": [ 2, "never" ],
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores( [
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ] ),
+] );
+
+export default eslintConfig;
