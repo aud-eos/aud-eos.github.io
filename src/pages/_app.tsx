9a827bef8c7b50e5110d16a4139df7ef0190cdd7
benny <22062601+benpetty@users.noreply.github.com>
2026-04-01 15:07:58 -0700

    update fonts and some colors throughtout app;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { Mona_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const fontMona_Sans = Mona_Sans({
+  subsets:[ "latin" ],
+});
+
 export default function App({ Component, pageProps }: AppProps ){
-  return <Component { ...pageProps } />;
+  return (
+    <div className={ fontMona_Sans.className }>
+      <Component { ...pageProps } />
+    </div>
+  );
 }
